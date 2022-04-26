bf285e4a3019bc605d695a4f0eafd52ee6af29da
zmuslu <zmuslu@KBN-BT07>
2022-04-26 15:56:23 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("MÜSTERİ EKLENDİ.\n");
+            Console.WriteLine("Müşteri TC: " + musteri.ID);
+            Console.WriteLine("Müşteri Adı Soyadı: " + musteri.Adi + " " + musteri.Soyadi);
+            Console.WriteLine("Müşteri Yaşı: " + musteri.Yasi);
+            Console.WriteLine("Müşteri Cinsiyeti: " + musteri.Cinsiyeti);
+            Console.WriteLine(" ");
+            Console.WriteLine("----------");
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("MÜŞTERİ SİLİNDİ.\n");
+            Console.WriteLine("Müşteri Adı Soyadı: " + musteri.Adi + " " + musteri.Soyadi);
+            Console.WriteLine(" ");
+            Console.WriteLine("-----------");
+        }
+
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri TC: " + musteri.ID);
+            Console.WriteLine("Müşteri Adı Soyadı: " + musteri.Adi + " " + musteri.Soyadi);
+            Console.WriteLine("Müşteri Yaşı: " + musteri.Yasi);
+            Console.WriteLine("Müşteri Cinsiyeti: " + musteri.Cinsiyeti);
+            Console.WriteLine(" ");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 1;
+            musteri1.Adi = "Ali";
+            musteri1.Soyadi = "Muslu";
+            musteri1.Yasi = 25;
+            musteri1.Cinsiyeti = "Erkek";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.ID = 2;
+            musteri2.Adi = "Zeynep";
+            musteri2.Soyadi = "Muslu";
+            musteri2.Yasi = 21;
+            musteri2.Cinsiyeti = "Kadın";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.ID = 3;
+            musteri3.Adi = "Hasan";
+            musteri3.Soyadi = "Muslu";
+            musteri3.Yasi = 52;
+            musteri3.Cinsiyeti = "Erkek";
+
+            Musteri musteri4 = new Musteri();
+            musteri4.ID = 4;
+            musteri4.Adi = "Hasibe";
+            musteri4.Soyadi = "Muslu";
+            musteri4.Yasi = 47;
+            musteri4.Cinsiyeti = "Kadın";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri3);
+            musteriManager.MusteriSil(musteri1);
+            Console.WriteLine("Müşteriler Listeleniyor.\n");
+            for (int i = 0; i <= musteriler.Length; i++)
+            {
+                musteriManager.MusteriListele(musteriler[i]);
+            }
+        }
+    }
+}
